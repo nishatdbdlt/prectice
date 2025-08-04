ba3d8618a5cf61c56383c3cab06c251206644b3a
nishatdbdlt <nishat7453@gmail.com>
2025-08-04 20:23:11 +0600

    this is me

---
+++ nishat.py
@@ -0,0 +1,56 @@
+# name = input("enter your name")
+# print("hello",name,"welcome")
+#
+#
+# age = input("enter you are")
+# print('hello',age,'year old')
+#
+#
+#
+# name = 'nishat'
+# age= 23
+# city ="dhaka"
+# print(name,age,city)
+
+
+# x ,y =input("enter two value").split()
+#
+# print("number of boys",x)
+# print("number of girls",y)
+#
+# x ,y ,z =input("enter you number").split()
+# print("enter number",x)
+# print("enter number",y)
+# print("enter number ",z)
+
+#
+# a = "hello world"
+# b = 10
+# c= 11.12
+# d=('nishat','nuri','rafi')
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+amount= 12.346
+print('amount:${:.8f}'.format(amount))
+
+print("python",end='@')
+print('nishat')
+
+#seprating with comma
+print('a','b','c',sep='')
+# format date
+print('02','04','1997',sep='-')
+print('nishat','hasibul',sep='@')
+
+name ="nishat"
+age = 23
+print(f"hello,my name is {name} i am {age} years old")
+
+
+num = int(input("inter value:"))
+add = (num + 4)
+print("the number is %d" %add)
