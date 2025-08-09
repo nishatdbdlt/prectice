b0d70712f739421f5652c51d536b9619c52bb7a5
nishatdbdlt <nishat7453@gmail.com>
2025-08-09 20:35:00 +0600

    array

---
+++ array.py
@@ -0,0 +1,35 @@
+arr = [5, 8, 2, 7, 1]
+
+total =0
+for num in arr:
+    total+= num
+print('total numner',total
+      )
+
+# large number:
+arr=[23,4,5,6,7,8]
+max_num=arr[0]
+if num > max_num:
+    max_num=num
+print('max number is ',max_num)
+
+#reverse arr:
+arr=[3,2,1,4,5,6]
+revers_arr=arr[ : :-1]
+print(revers_arr)
+
+arr = [10, 20, 30, 40, 50, 60]
+resvese_arr=arr[1::4]
+print(resvese_arr)
+
+arr = [10, 20, 30, 40, 50, 60]
+nishat_arr=arr[-2:]
+
+nishat_arr=arr[:: -1]
+print(nishat_arr)
+
+
+arr = [10, 20, 30, 40, 50, 60]
+print(arr[4:1:-1])
+print(arr[1:3:2])
+print(arr[::])
